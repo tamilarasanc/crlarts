db87debe26309fa5d26432f3b64836a2b4b5f1a7
tamilarasan <tamilarasanc@kreatio.com>
2016-07-22 09:44:59 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crlarts::Application.config.secret_token = 'caa6006663e137c39eba1be6ee7cefd98b6e021123dd1c5113e2130d32fdffd69d2ea83f3f1dd870274affb4d39e68d3d69146f513f586f61f9a5ae8651117d0'
